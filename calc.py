97e478dca87c93088e9b9d475878134adbb530ba
alpha-martinez <martinezalphaa@gmail.com>
2020-09-24 15:08:08 -0400

    adding calc.py to my github

---
+++ calc.py
@@ -0,0 +1,19 @@
+def add(x, y):
+    results = x + y
+    return results
+    # add function
+
+def subtract(x, y):
+    results = x - y
+    return results
+    #  subtract function
+
+def multiply(x, y):
+    results = x * y
+    return results
+
+def divide(x, y):
+    results = x / y
+    return results
+
+print(add(4, 2))
